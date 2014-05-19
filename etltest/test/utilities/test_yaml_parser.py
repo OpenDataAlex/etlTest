edb5331adf017c6e2ca8ee796c79c348dbb76142
ameadows <alexmeadows@bluefiredatasolutions.com>
2014-05-19 19:50:20 -0400

    Finally got all yaml parser tests working again.

--- etltest/test/utilities/test_yaml_parser.py
+++ etltest/test/utilities/test_yaml_parser.py
@@ -5,7 +5,6 @@
 __author__ = 'ameadows'
 
 import unittest
-import json
 
 from etltest.utilities.yaml_parser import YAMLParser
 from etltest.utilities.settings_manager import SettingsManager
@@ -25,20 +24,21 @@ class YamlParserTests(unittest.TestCase):
     def test_process_test_file(self):
         given_result = YAMLParser().read_file(test_file)
         expected_result = [{'DataMart\\Users': [{'suites': [{'suite': 'dataMart'}, {'suite': 'userDim'}]
-                        , 'desc': 'Test for process that lower cases the first name field of a users table record.'
-                        , 'query': [{'from': 'users', 'where': 'user_id = 2', 'result': 'sarah'
-                        , 'select': 'first_name'}], 'name': 'testFirstNameLower', 'dataset': [{'source': 'etlUnitTest'
-                        , 'table': 'users', 'records': [1]}]}, {'suites': [{'suite': 'dataMart'}, {'suite': 'userDim'}]
-                        , 'desc': 'Test for process that upper cases the first name field of a users table record.'
-                        , 'query': [{'from': 'users', 'where': 'user_id = 2', 'result': 'SARAH'
-                        , 'select': 'first_name'}], 'name': 'testFirstNameUpper', 'dataset': [{'source': 'etlUnitTest'
-                        , 'table': 'users', 'records': [1]}]}, {'suites': [{'suite': 'userDim'}]
-                        , 'desc': 'Test for valid birth dates.', 'query': [{'from': 'users'
-                        , 'where': 'user_id IN (1, 2)', 'result': [{1: '01-01-1900'}, {2: '02-02-2000'}]
-                        , 'select': 'birthday'}], 'name': 'testUserValidBirthday', 'dataset': [{'source': 'etlUnitTest'
-                        , 'table': 'users', 'records': [1, 2]}]}]}]
+                          , 'desc': 'Test for process that lower cases the first name field of a users table record.'
+                          , 'query': [{'from': 'users', 'where': 'user_id = 2', 'result': 'sarah'
+                          , 'select': 'first_name'}], 'name': 'testFirstNameLower', 'dataset': [{'source': 'etlUnitTest'
+                          , 'table': 'users', 'records': [1]}]}, {'suites': [{'suite': 'dataMart'}
+                          , {'suite': 'userDim'}]
+                          , 'desc': 'Test for process that upper cases the first name field of a users table record.'
+                          , 'query': [{'from': 'users', 'where': 'user_id = 2', 'result': 'SARAH'
+                          , 'select': 'first_name'}], 'name': 'testFirstNameUpper', 'dataset': [{'source': 'etlUnitTest'
+                          , 'table': 'users', 'records': [1]}]}, {'suites': [{'suite': 'userDim'}]
+                          , 'desc': 'Test for valid birth dates.', 'query': [{'from': 'users'
+                          , 'where': 'user_id IN (1, 2)', 'result': [{1: '01-01-1900'}, {2: '02-02-2000'}]
+                          , 'select': 'birthday'}], 'name': 'testUserValidBirthday'
+                          , 'dataset': [{'source': 'etlUnitTest', 'table': 'users', 'records': [1, 2]}]}]}]
 
-        self.assertEqual(given_result, expected_result)
+        self.assertItemsEqual(given_result, expected_result)
 
     # Testing data yaml file for correct processing.
     def test_process_data_file(self):
@@ -46,7 +46,7 @@ class YamlParserTests(unittest.TestCase):
         expected_result = [{1: {"first_name": "Bob", "last_name": "Richards", "birthday": "01-40-2000",
                            "zipcode": 55555}, 2: {"first_name": "Sarah", "last_name": "Jenkins",
                            "birthday": "02-02-2000", "zipcode": 12345}}]
-        self.assertEqual(given_result, expected_result)
+        self.assertItemsEqual(given_result, expected_result)
 
 
     # Testing a directory of test yaml files to verify they are processed correctly.

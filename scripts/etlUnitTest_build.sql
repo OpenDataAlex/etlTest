7c7f699811baf0e064f6cc0d871fb4ef5b4ec625
Alex Meadows <ameadows@redhat.com>
2014-06-11 11:04:54 -0400

    Trying out Travis-CI's built in database functionality to resolve some of the errors occurring.

---
+++ scripts/etlUnitTest_build.sql
@@ -0,0 +1,16 @@
+USE etlUnitTest;
+
+
+CREATE TABLE users (
+                user_id INT NOT NULL,
+                first_name VARCHAR(75) NOT NULL,
+                last_name VARCHAR(75) NOT NULL,
+                birthday DATE NOT NULL,
+                zipcode CHAR(5) NOT NULL,
+                PRIMARY KEY (user_id)
+);
+
+
+CREATE UNIQUE INDEX users_idx
+ ON users
+ ( user_id );

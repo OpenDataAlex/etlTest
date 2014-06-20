9c1c09f75e57f131f8d688cfaff196e801908ad6
Alex Meadows <ameadows@redhat.com>
2014-06-20 16:28:46 -0400

    Started bare bones code for code executor.

--- etltest/code_executor.py
+++ etltest/code_executor.py
@@ -1,4 +1,20 @@
 __author__ = 'ameadows'
+"""
+    This file contains all the logic necessary to execute ETL code based on the settings stored in the tools settings
+    file.  Code execution should occur after the test data is loaded and before the unit tests are run.
+"""
 
+from etltest.utilities.settings import etltest_config, console
 
 class CodeExecutor():
+
+    def __init__(self):
+        """
+            Here we initialize the CodeExecutor object, setting up the executor per the settings in the tool settings
+            file.
+        """
+
+        self.log = logging.getLogger(name="CodeExecutor")
+        self.log.setLevel(etltest_config['logging_level'])
+        self.log.addHandler(console)
+

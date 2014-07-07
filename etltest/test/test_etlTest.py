69f771b6bb44e6324034244b5d634e9a2225e1ac
Alex Meadows <ameadows@redhat.com>
2014-07-07 16:26:14 -0400

    Had to add dummy test for etlTest.

--- etltest/test/test_etlTest.py
+++ etltest/test/test_etlTest.py
@@ -7,14 +7,18 @@ from etltest.utilities.settings_manager import SettingsManager
 
 class etlTestTests(unittest.TestCase):
 
-    # def setUp(self):
-    #     self.env = scripttest.TestFileEnvironment('./test-output')
-    #     self.process = "../../etlTest.py"
-    #
-    #     self.in_file = SettingsManager().system_variable_replace("${"
-    #                                                              "ETL_TEST_ROOT}/etltest/samples/test/dataMart/users_dim.yml")
-    #     self.in_dir = SettingsManager().system_variable_replace("${ETL_TEST_ROOT}/etltest/samples/test/dataMart/")
-    #
+    def setUp(self):
+        self.env = scripttest.TestFileEnvironment('./test-output')
+        self.process = "../../etlTest.py"
+
+        self.in_file = SettingsManager().system_variable_replace("${"
+                                                                 "ETL_TEST_ROOT}/etltest/samples/test/dataMart/users_dim.yml")
+        self.in_dir = SettingsManager().system_variable_replace("${ETL_TEST_ROOT}/etltest/samples/test/dataMart/")
+
+    def test_setup(self):
+        # Had to create this to test if the executor would build correctly during setup.
+        self.assertEqual(True, True)
+
     # def test_with_empty_args(self):
     #     process = str(self.env.run("python", self.process, "-h"))
     #     given_result = process["stdout"]

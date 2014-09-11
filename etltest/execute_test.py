e93188917e59415e6cdd5de5e46270a3cd9349a4
Alex Meadows <ameadows@cspring.com>
2014-09-11 15:14:13 -0400

    Trying to remove another module.

--- etltest/execute_test.py
+++ etltest/execute_test.py
@@ -5,7 +5,6 @@ import subprocess
 class executeTest():
 
     def execute(self):
-        #p = subprocess.Popen(['echo', 'Hello!'], stdout=subprocess.PIPE)
         p = subprocess.Popen(['python', '/home/ameadows/Documents/etlTest/output/DataMart/UsersDim.py'], stdout=subprocess.PIPE)
 
         print p.communicate()
